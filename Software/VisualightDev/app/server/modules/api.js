0206719b7b4e098a504045932c16a324c260fd17
lpercifield@gmail.com <lpercifield@gmail.com>
2013-09-26 15:40:43 -0400

    fixed NaN from hue2rgb

--- Software/VisualightDev/app/server/modules/api.js
+++ Software/VisualightDev/app/server/modules/api.js
@@ -103,7 +103,7 @@ var processBulbColors = function(bulbObject){
             if(t < 1/6) return p + (q - p) * 6 * t;
             if(t < 1/2) return q;
             if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
+            return parseInt(p);
         }
 
         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
