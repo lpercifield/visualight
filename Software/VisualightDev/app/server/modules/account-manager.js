0439d9372340a28781afb54b94c138bd784b3346
chris.piuggi <chris@piuggi.com>
2013-11-06 11:32:05 -0500

    colors test

--- Software/VisualightDev/app/server/modules/account-manager.js
+++ Software/VisualightDev/app/server/modules/account-manager.js
@@ -177,7 +177,7 @@ exports.addNewGroup = function(user, post, callback){
 
 	//collect post data 
 	//process post data
-	console.log(post+"".error);
+	console.log(JSON.stringify(post).error+"".error);
 
 	//check group name 
 
