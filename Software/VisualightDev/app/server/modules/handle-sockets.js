3204199d90ad27ee1a3dc2b5a88dd6ba766886dd
chris.piuggi <chris@piuggi.com>
2013-11-06 19:32:03 -0500

    bulbObject Reporting

--- Software/VisualightDev/app/server/modules/handle-sockets.js
+++ Software/VisualightDev/app/server/modules/handle-sockets.js
@@ -174,7 +174,7 @@ exports.createSockets = function(app, io, AM){
         */
 
         function sendToVisualight(bulbObject,heartbeat){
-        		console.log(bulbObject)
+        		console.log(bulbObject);
                 var data = bulbObject.color.r+","+bulbObject.color.g+","+bulbObject.color.b+","+bulbObject.color.w; // this creates the r,g,b,blink array
                 
                 heartbeat = typeof heartbeat !== 'undefined' ? heartbeat : false; // if we didnt define heartbeat then set it to false
