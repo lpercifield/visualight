378064eb2a4d405edef6e5aa5c82f461966eb6cd
lpercifield@gmail.com <lpercifield@gmail.com>
2013-11-22 11:09:33 -0500

    Added comments to begin working on #22

--- Software/VisualightDev/app/server/modules/auth.js
+++ Software/VisualightDev/app/server/modules/auth.js
@@ -11,7 +11,7 @@ exports.sessionCheck = function(req, res, next){
 }
 
 //returns 400
-exports.authCheck = function(req, res, next){
+exports.authCheck = function(req, res, next){ // This should be able to handle both a session/cookie auth or an api key auth
 	console.log('Checking Auth');
 	AM.sessionAuth(req.cookies['connect.sid'],req.session, function(valid){
 		if(valid === true) next();
